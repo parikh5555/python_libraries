139dbf4b8d5ff3aff23dabfdc5c0de4266517499
Abhishek Parikh <abhishekparikh.ospl.com>
2018-08-09 10:39:28 +0530

    pandas update NaN to 0

--- Pandas_func.py
+++ Pandas_func.py
@@ -7,13 +7,16 @@ rf = pd.read_csv('csvfile.csv', index_col = False, header = 0)
 print rf
 print rf.values
 
-for i in range(0,len(rf.values)):
-    for j in range(0,len(rf[0])):
-        if data[i][j].isnull :
-            count = count +1
-            rf.values[i][j] = 0
+rf = rf.fillna(0)
+print rf
 
-
+##for i in range(0,len(rf.values)):
+##    for j in range(0,len(rf.values[0])):
+##        if np.isnan(rf.values[i][j]) :
+##            count = count +1
+##            rf.values[i][j] = 0.0
+##print count
+##print rf.values
 ##data = np.array(rf)
 ##print data
 ##
